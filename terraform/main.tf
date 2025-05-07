350091b2861081f53c0b168b32e9f9702f25cab0
Anne0104 <labouretcatherine@gmail.com>
2025-05-07 12:23:28 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,13 @@
-provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-  skip_credentials_validation = true
-  endpoints {
-    ec2 = "http://ip10-0-4-4-d0dgq9g05akh4glkf8p0-4566.direct.lab-boris.fr"
-  }
+variable "ami_id" {
+  type    = string
+  default = "ami-12345678"
 }
 
 resource "aws_instance" "demo" {
-  ami           = "ami-12345678"
+  ami           = var.ami_id
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance-${timestamp()}"
+  }
 }
