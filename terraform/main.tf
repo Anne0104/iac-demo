dad7306d30082eb30ed3d09f7c333748e237e2da
Anne0104 <labouretcatherine@gmail.com>
2025-05-07 12:54:31 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-1-11-cvh6b3qb9qb14bivkpfg-4566.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-4-4-d0dgq9g05akh4glkf8p0-4566.direct.lab-boris.fr"
   }
 }
 
